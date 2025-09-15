270269440a529c2a20d19fbbafcf15c4882ae8cf
Sylvain Goumy <goumsss@protonmail.com>
2025-09-15 17:53:10 +0200

    chore: add bepolia

--- ponder.config.ts
+++ ponder.config.ts
@@ -5,6 +5,10 @@ import { AutomateAbi } from "./abis/AutomateAbi";
 export default createConfig({
 	ordering: "multichain",
 	chains: {
+		bepolia: {
+			id: 80069,
+			rpc: process.env.PONDER_RPC_URL_80069,
+		},
 		berachain: {
 			id: 80094,
 			rpc: process.env.PONDER_RPC_URL_80094,
@@ -14,6 +18,10 @@ export default createConfig({
 		Automate: {
 			abi: AutomateAbi,
 			chain: {
+				bepolia: {
+					address: "0xafd37d0558255aA687167560cd3AaeEa75c2841E",
+					startBlock: 1792563,
+				},
 				berachain: {
 					address: "0xafd37d0558255aA687167560cd3AaeEa75c2841E",
 					startBlock: 702100,
