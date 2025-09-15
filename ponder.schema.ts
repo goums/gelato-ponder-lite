c9c7b40d6bc2f866e5626a7ee21a73ad31ada8f5
Sylvain Goumy <goumsss@protonmail.com>
2025-09-15 11:14:55 +0200

    feat: index Automate on berachain

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,6 +1,54 @@
-import { onchainTable } from "ponder";
+import { onchainTable, primaryKey, relations } from "ponder";
 
-export const account = onchainTable("account", (t) => ({
-  address: t.hex().primaryKey(),
-  balance: t.bigint().notNull(),
+export const tasks = onchainTable(
+	"tasks",
+	(t) => ({
+		id: t.text(),
+		chainId: t.integer(),
+		taskHash: t.hex().notNull(),
+		taskCreatorId: t.hex().notNull(),
+		status: t.text().notNull(),
+		nbExecutions: t.integer().notNull(),
+		modules: t.integer().array().notNull(),
+		moduleArgs: t.hex().array().notNull(),
+		createdAt: t.bigint().notNull(),
+		cancelledAt: t.bigint(),
+	}),
+	(table) => ({ pk: primaryKey({ columns: [table.chainId, table.id] }) }),
+);
+
+export const executions = onchainTable(
+	"executions",
+	(t) => ({
+		id: t.hex(),
+		chainId: t.integer(),
+		taskId: t.text().notNull(),
+		executedAt: t.bigint().notNull(),
+		transactionHash: t.hex().notNull(),
+	}),
+	(table) => ({ pk: primaryKey({ columns: [table.chainId, table.id] }) }),
+);
+
+export const creators = onchainTable("creators", (t) => ({
+	id: t.hex().primaryKey(),
+	nbTasks: t.integer().notNull(),
+}));
+
+export const taskRelations = relations(tasks, ({ many, one }) => ({
+	executions: many(executions),
+	taskCreator: one(creators, {
+		fields: [tasks.taskCreatorId],
+		references: [creators.id],
+	}),
+}));
+
+export const taskExecutionRelations = relations(executions, ({ one }) => ({
+	task: one(tasks, {
+		fields: [executions.taskId],
+		references: [tasks.id],
+	}),
+}));
+
+export const taskCreatorRelations = relations(creators, ({ many }) => ({
+	tasks: many(tasks),
 }));
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,104 @@
-import { ponder } from "ponder:registry";
-import { account } from "ponder:schema";
+import { ponder, type IndexingFunctionArgs } from "ponder:registry";
+import { tasks, executions, creators } from "ponder:schema";
 
-ponder.on("weth9:Deposit", async ({ event, context }) => {
-  await context.db
-    .insert(account)
-    .values({ address: event.args.dst, balance: event.args.wad })
-    .onConflictDoUpdate((row) => ({ balance: row.balance + event.args.wad }));
-});
+ponder.on("Automate:TaskCreated", onTaskCreated);
+ponder.on("Automate:TaskCancelled", onTaskCancelled);
+
+ponder.on("Automate:ExecBypassModuleSuccess", onExecSucess);
+ponder.on("Automate:ExecBypassModuleSyncFeeSuccess", onExecSucess);
+ponder.on("Automate:ExecSuccess", onExecSucess);
+
+export async function onExecSucess({
+	event,
+	context,
+}: IndexingFunctionArgs<
+	| "Automate:ExecBypassModuleSyncFeeSuccess"
+	| "Automate:ExecBypassModuleSuccess"
+	| "Automate:ExecSuccess"
+>) {
+	console.log(`ExecSuccess: ${event.args.taskId}`);
+	const db = context.db;
+	const chainId = context.chain.id;
+
+	await db.insert(executions).values({
+		id: event.transaction.hash,
+		chainId,
+		taskId: event.args.taskId,
+		executedAt: event.block.timestamp,
+		transactionHash: event.transaction.hash,
+	});
+
+	await db
+		.update(tasks, {
+			chainId,
+			id: event.args.taskId,
+		})
+		.set((task) => ({
+			nbExecutions: task.nbExecutions + 1,
+		}));
+}
+
+export async function onTaskCreated({
+	event,
+	context,
+}: IndexingFunctionArgs<"Automate:TaskCreated">) {
+	const db = context.db;
+	const chainId = context.chain.id;
+	console.log(`TaskCreated: ${chainId} ${event.args.taskId}`);
+
+	await db
+		.insert(creators)
+		.values({
+			id: event.args.taskCreator,
+			nbTasks: 1,
+		})
+		.onConflictDoUpdate((creator) => ({
+			nbTasks: creator.nbTasks + 1,
+		}));
+
+	await db
+		.insert(tasks)
+		.values({
+			id: event.args.taskId,
+			chainId,
+			taskHash: event.args.taskId,
+			taskCreatorId: event.args.taskCreator,
+			status: "ongoing",
+			nbExecutions: 0,
+			modules: [...event.args.moduleData.modules],
+			moduleArgs: [...event.args.moduleData.args],
+			createdAt: event.block.timestamp,
+		})
+		.onConflictDoUpdate((task) => ({
+			status: "ongoing",
+			createdAt: event.block.timestamp,
+		}));
+}
+
+export async function onTaskCancelled({
+	event,
+	context,
+}: IndexingFunctionArgs<"Automate:TaskCancelled">) {
+	const db = context.db;
+	const chainId = context.chain.id;
+
+	console.log(`TaskCancelled: ${chainId} ${event.args.taskId}`);
+
+	await db
+		.update(creators, {
+			id: event.args.taskCreator,
+		})
+		.set((creator) => ({
+			nbTasks: creator.nbTasks - 1,
+		}));
+
+	await db
+		.update(tasks, {
+			chainId,
+			id: event.args.taskId,
+		})
+		.set((task) => ({
+			status: "cancelled",
+			cancelledAt: event.block.timestamp,
+		}));
+}
