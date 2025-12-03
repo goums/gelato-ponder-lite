5b6b47c1db954843f18053f4a69d968d541ba963
Sylvain Goumy <goumsss@protonmail.com>
2025-12-03 15:31:28 +0100

    chore: remove bepolia

--- ponder.config.ts
+++ ponder.config.ts
@@ -9,10 +9,6 @@ export default createConfig({
 			id: 42161,
 			rpc: process.env.PONDER_RPC_URL_42161,
 		},
-		bepolia: {
-			id: 80069,
-			rpc: process.env.PONDER_RPC_URL_80069,
-		},
 		berachain: {
 			id: 80094,
 			rpc: process.env.PONDER_RPC_URL_80094,
@@ -21,10 +17,10 @@ export default createConfig({
 			id: 11155111,
 			rpc: process.env.PONDER_RPC_URL_11155111,
 		},
-		/*baseSepolia: {
+		baseSepolia: {
 			id: 84532,
 			rpc: process.env.PONDER_RPC_URL_84532,
-		},*/
+		},
 		arbSepolia: {
 			id: 421614,
 			rpc: process.env.PONDER_RPC_URL_421614,
@@ -38,10 +34,6 @@ export default createConfig({
 					address: "0x2A6C106ae13B558BB9E2Ec64Bd2f1f7BEFF3A5E0",
 					startBlock: 58563527,
 				},
-				bepolia: {
-					address: "0xafd37d0558255aA687167560cd3AaeEa75c2841E",
-					startBlock: 1792563,
-				},
 				berachain: {
 					address: "0xafd37d0558255aA687167560cd3AaeEa75c2841E",
 					startBlock: 702100,
@@ -50,10 +42,10 @@ export default createConfig({
 					address: "0x2A6C106ae13B558BB9E2Ec64Bd2f1f7BEFF3A5E0",
 					startBlock: 4612086,
 				},
-				/*baseSepolia: {
+				baseSepolia: {
 					address: "0x2A6C106ae13B558BB9E2Ec64Bd2f1f7BEFF3A5E0",
 					startBlock: 4638568,
-				},*/
+				},
 				arbSepolia: {
 					address: "0x2A6C106ae13B558BB9E2Ec64Bd2f1f7BEFF3A5E0",
 					startBlock: 1096654,
